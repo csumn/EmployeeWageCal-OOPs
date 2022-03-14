db2559237fca93c7e2424bbc3acb0733a505b092
csumn <csumn13@gmail.com>
2022-03-14 21:07:16 +0530

    Part time Emp wage code added

--- EmployeeWageCalculation/src/com/brodgelabs/EmployeeWage.java
+++ EmployeeWageCalculation/src/com/brodgelabs/EmployeeWage.java
@@ -1,14 +1,19 @@
 package com.brodgelabs;
 
 public class EmployeeWage {
+	static final int IS_FULL_TIME=1;
+	static final int IS_PART_TIME=2;
 
-	static void dailyWage(int wagePerHour, int empHrs) {
-		int isPresent = 1;
-		int value = (int)Math.floor(Math.random()*10) % 2;
-		if(value == isPresent) {
-			int empWage = wagePerHour * empHrs; 
-			System.out.println("Employee is Present and Wage is :" +empWage);
-		}else
-			System.out.println("Employee is absent today.....");
+	static void dailyWage(int wagePerHour, int fullTimeEmpHrs, int partTimeEmpHrs) {
+		int value = (int)Math.floor(Math.random()*10) % 3;
+		int empWage=0;
+		if(value == IS_FULL_TIME) {
+			empWage = wagePerHour * fullTimeEmpHrs; 
+			System.out.println("Employee is Present Full Day and Wage is : " +empWage);
+		}else if(value == IS_PART_TIME) {
+			empWage = wagePerHour * partTimeEmpHrs; 
+			System.out.println("Employee is Present Half Day and Wage is : " +empWage);
+		}
+		else System.out.println("Employee is Absent today, as the reason today's wage is : "+empWage);
 	}
 }
