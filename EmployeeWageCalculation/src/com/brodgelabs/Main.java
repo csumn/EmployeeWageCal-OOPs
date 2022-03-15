0a0afde608a42e8e8e643693bf800515bda98484
csumn <csumn13@gmail.com>
2022-03-15 20:06:12 +0530

    Emp Wage condition pogram added

--- EmployeeWageCalculation/src/com/brodgelabs/Main.java
+++ EmployeeWageCalculation/src/com/brodgelabs/Main.java
@@ -1,16 +1,8 @@
 package com.brodgelabs;
 
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Welcome to EmployeeWage Computation..... \n");
-		System.out.println("Enter \n  Employee Wage per hour \n  Employee Full Time Hour \n  Part Time Hour");
-		Scanner scanner = new Scanner(System.in);
-		int wagePerHour = scanner.nextInt();
-		int fullTimeEmpHrs = scanner.nextInt();
-		int partTimeEmpHrs = scanner.nextInt();
-		EmployeeWage.dailyWage(wagePerHour, fullTimeEmpHrs, partTimeEmpHrs);
-		scanner.close();
+		EmployeeWage employeeWage = new EmployeeWage();
+		employeeWage.calculateWage();
 	}
 }
