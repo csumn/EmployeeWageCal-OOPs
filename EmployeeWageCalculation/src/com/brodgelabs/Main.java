c585405b445468d4706e1d6b4c404cb6323e979b
csumn <csumn13@gmail.com>
2022-03-16 20:02:20 +0530

    sal of each company storing

--- EmployeeWageCalculation/src/com/brodgelabs/Main.java
+++ EmployeeWageCalculation/src/com/brodgelabs/Main.java
@@ -4,8 +4,17 @@ public class Main {
 
 	public static void main(String args[])
 	{
-		EmployeeWage employeeWage = new EmployeeWage();
-		employeeWage.calculateTotalWage("Amazon", 200, 20, 180);
-		employeeWage.calculateTotalWage("Delloite", 150, 20, 200);
+		EmployeeWage employeeWage = new EmployeeWage("Amazon", 300, 25, 200);
+		EmployeeWage employeeWage2 = new EmployeeWage("Delloite", 250, 30, 200);
+		EmployeeWage employeeWage3 = new EmployeeWage("Microsoft", 280, 28, 200);
+
+		employeeWage.calculateTotalWage();
+		employeeWage.display();
+
+		employeeWage2.calculateTotalWage();
+		employeeWage2.display();
+
+		employeeWage3.calculateTotalWage();
+		employeeWage3.display();
 	}
 }
